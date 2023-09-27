92e4f5bb6ab828f2ad547f8d37a781252c396414
Sandhya110 <starsandhya369@gmail.com>
2023-09-27 10:26:58 +0530

    Random color generator

---
+++ 12_random_color_generator/script.js
@@ -0,0 +1,21 @@
+
+const getColor = () => {
+  // Hex Code
+  const randomNumber = Math.floor(Math.random() * 16777215)
+  const randomCode = "#" + randomNumber.toString(16);
+  document.body.style.backgroundColor = randomCode;
+  document.getElementById('color-code').innerText = randomCode;
+
+  // copy color code
+  navigator.clipboard.writeText(randomCode);
+}
+
+// event call
+document.getElementById("btn").addEventListener(
+  'click', 
+getColor
+
+)
+
+// init call
+getColor();
